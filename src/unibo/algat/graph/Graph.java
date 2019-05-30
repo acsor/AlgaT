8fe3cbb197669a0ac588f503467cf94e5c337cb3
Acsor <nildexo@yandex.com>
2019-05-30 11:29:27 +0200

    Refine interface of Graph to allow traversing order

--- src/unibo/algat/graph/Graph.java
+++ src/unibo/algat/graph/Graph.java
@@ -1,7 +1,6 @@
 package unibo.algat.graph;
 
 import java.util.Comparator;
-import java.util.Set;
 import java.util.SortedSet;
 
 /**
@@ -11,50 +10,50 @@ import java.util.SortedSet;
 public interface Graph<T> {
     /**
      * @param node Node<T> instance to insert into the current graph. If
-     *             already present, the graph is not affected. If null,
-     *             undefined behavior may occur.
+     *             already present, the graph is unaffected.
+     * @throws java.lang.NullPointerException if {@code node} happens to be
+     * {@code null}
      */
     void insertNode(Node<T> node);
     /**
      * @param node Node<T> instance to delete from the graph. If not present,
-     *            the graph is unaffected and thus delete() is a no-op.
+     *             the graph is unaffected and thus delete() is a no-op.
      *             If null, undefined behavior may occur.
      */
     void deleteNode(Node<T> node);
 
     /**
      * @return The set of vertices contained in the graph, empty if none are
-     * present. If either argument is null, undefined behavior can occur.
+     * present.
      */
-    Set<Node<T>> vertices();
-    /**
-     * @param order User-defined criteria by which the vertices should be sorted
-     * @return An ordered set of vertices composing the nodes of the graph,
-     * ordered according to {@code order}.
-     */
-    SortedSet<Node<T>> vertices(Comparator<Node<T>> order);
+    SortedSet<Node<T>> vertices();
 
     /**
      * @param node Node value from which the adjacent nodes should be retrieved
      * @return A set of nodes v such that, if u is the current node, then the
-     * (u, v) edge belongs to the graph. Alternatively, all nodes reachable
-     * from the input one.
+     * {@code (u, v)} edge belongs to the graph. Alternatively, all nodes
+     * reachable from the given one.
+     * @throws java.util.NoSuchElementException if {@code node} is not found
+     * within the graph or is {@code null}
      */
-    Set<Node<T>> adjacents(Node<T> node);
-    /**
-     * @param node Node value from which the adjacent nodes should be retrieved
-     * @param order Order by which the adjacent nodes should be returned
-     * @return An ordered set of vertices adjacent to {@code node}
-     */
-    SortedSet<Node<T>> adjacents(Node<T> node, Comparator<Node<T>> order);
+    SortedSet<Node<T>> adjacents(Node<T> node);
 
     /**
-     * Inserts an edge (a, b) into the graph.
+     * Inserts an {@code (a, b)} edge into the graph.
+     * @throws java.util.NoSuchElementException if either {@code a} or {@code b}
+     * aren't within the graph
      */
     void insertEdge(Node<T> a, Node<T> b);
     /**
-     * Delete the edge (a, b) from the graph, if present. If not, this is a
-     * no-op. If either argument is null, undefined behavior can occur.
+     * Delete the {@code (a, b)} edge from the graph, if present. If not, or if
+     * the {@code a} or {@code b} nodes aren't in the graph, this is a
+     * no-op.
      */
     void deleteEdge(Node<T> a, Node<T> b);
+
+    /**
+     * @return The {@code Comparator<Node<T>>} instance used to order the
+     * vertices in this graph.
+     */
+    Comparator<Node<T>> comparator ();
 }
