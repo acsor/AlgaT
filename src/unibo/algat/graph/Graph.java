060fe29bea7645944491fbf4338cdea08ea17473
Acsor <nildexo@yandex.com>
2019-05-31 17:21:57 +0200

    Refine Graph methods specification

--- src/unibo/algat/graph/Graph.java
+++ src/unibo/algat/graph/Graph.java
@@ -11,14 +11,15 @@ public interface Graph<T> {
     /**
      * @param node Node<T> instance to insert into the current graph. If
      *             already present, the graph is unaffected.
-     * @throws java.lang.NullPointerException if {@code node} happens to be
-     * {@code null}
+     * @throws java.lang.IllegalArgumentException if {@code node} happens to be
+     * {@code null}.
      */
     void insertNode(Node<T> node);
     /**
-     * @param node Node<T> instance to delete from the graph. If not present,
-     *             the graph is unaffected and thus delete() is a no-op.
-     *             If null, undefined behavior may occur.
+     * @param node Node<T> instance to delete from the graph. If not present the
+     *             graph is unaffected and thus {@code delete()} is a no-op.
+     * @throws java.lang.IllegalArgumentException if {@code node} happens to be
+     * {@code null}.
      */
     void deleteNode(Node<T> node);
 
@@ -27,27 +28,33 @@ public interface Graph<T> {
      * present.
      */
     SortedSet<Node<T>> vertices();
-
     /**
-     * @param node Node value from which the adjacent nodes should be retrieved
+     * @param node Node value of which a list of adjacent nodes is queried for.
      * @return A set of nodes v such that, if u is the current node, then the
      * {@code (u, v)} edge belongs to the graph. Alternatively, all nodes
      * reachable from the given one.
      * @throws java.util.NoSuchElementException if {@code node} is not found
-     * within the graph or is {@code null}
+     * within the graph.
+     * @throws java.lang.IllegalArgumentException if {@code node} is {@code
+     * null}.
      */
     SortedSet<Node<T>> adjacents(Node<T> node);
 
     /**
-     * Inserts an {@code (a, b)} edge into the graph.
+     * Inserts an {@code (a, b)} edge into the graph. Produces no change if
+     * {@code (a, b)} was already present.
      * @throws java.util.NoSuchElementException if either {@code a} or {@code b}
      * aren't within the graph
+     * @throws java.lang.IllegalArgumentException if either {@code a} or
+     * {@code b} was {@code null}.
      */
     void insertEdge(Node<T> a, Node<T> b);
     /**
      * Delete the {@code (a, b)} edge from the graph, if present. If not, or if
      * the {@code a} or {@code b} nodes aren't in the graph, this is a
      * no-op.
+     * @throws java.lang.IllegalArgumentException if either {@code a} or
+     * {@code b} was {@code null}.
      */
     void deleteEdge(Node<T> a, Node<T> b);
 
@@ -55,5 +62,5 @@ public interface Graph<T> {
      * @return The {@code Comparator<Node<T>>} instance used to order the
      * vertices in this graph.
      */
-    Comparator<Node<T>> comparator ();
+    Comparator<Node<T>> comparator();
 }
