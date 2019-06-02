ad9ec0f5d7dc3a7bff60ed8f745a6d653503bff6
Acsor <nildexo@yandex.com>
2019-06-02 15:07:31 +0200

    Add skeleton for ShortestPathLesson class

---
+++ src/unibo/algat/ShortestPathLesson.java
@@ -0,0 +1,7 @@
+package unibo.algat;
+
+public class ShortestPathLesson extends Lesson {
+    public ShortestPathLesson() {
+        super(ShortestPathLesson.class.getSimpleName(), "Graph problems");
+    }
+}
