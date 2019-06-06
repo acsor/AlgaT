5dfaf2e7a4ea6c72465c23ea79c20fe1ac344749
Acsor <nildexo@yandex.com>
2019-06-06 15:51:10 +0200

    Leave hints as to how implement MapWeightFunction

--- src/unibo/algat/graph/MapWeightFunction.java
+++ src/unibo/algat/graph/MapWeightFunction.java
@@ -1,13 +1,18 @@
 package unibo.algat.graph;
 
+import unibo.algat.util.Pair;
+
+import java.util.Map;
+
 /**
- * A memory-based weight function, assigning weight to a graph based on
- * stored data.
- * @param <T>
- * @param <W>
+ * A memory-based weight function, assigning weight to a graph based on stored
+ * data.
+ * @param <T> Type of the nodes composing the graph.
+ * @param <W> Type of the weight value to assign to the graph edges.
  */
 public class MapWeightFunction<T, W extends Number> implements
 	WeightFunction<T, W> {
+	private Map<Pair<Node<T>, Node<T>>, W> mWeights;
 
 	@Override
 	public W weight(Node<T> a, Node<T> b) {
