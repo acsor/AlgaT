6c29e6aeb261441d060bd9a99d21eb38454673b5
Paolo Marzolo <pollo.marzo@gmail.com>
2019-07-03 21:23:49 +0200

    added initial version of preferences window
    not sure how to deal with locale. i believe the best choice is to change the default one, but still unsure on how to proceed
    Opted for a Save method instead of a ChangeListener since it'd be pretty useless
    GridPane seems to be a good fit! Also included a css file to try that out. Seems fun

--- src/main/java/unibo/algat/control/PreferencesController.java
+++ src/main/java/unibo/algat/control/PreferencesController.java
@@ -1,19 +1,40 @@
 package unibo.algat.control;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 
+import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 public class PreferencesController {
     private final ResourceBundle mInterface;
-    @FXML private Label mLanguageField;
+    @FXML private ChoiceBox<String> mLanguageChoice;
 
 
 
     public PreferencesController() {
         mInterface = ResourceBundle.getBundle("Interface");
+    }
+
+    @FXML
+    private void initialize() {
+        String[] e = mInterface.getString(
+                "gui.dialog.preferences.languageSelection").split(",");
+        for (String i : e)
+            mLanguageChoice.getItems().add(i);
+
+    }
+
+    @FXML
+    private void savePressed() {
+
+        Locale.setDefault(Locale.ITALIAN);
+    }
+
+    @FXML
+    private void cancelPressed() {
 
     }
 
