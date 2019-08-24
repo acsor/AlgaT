c3ca5215b33717d86af17e4648ad445ab84fddff
Acsor <nildexo@yandex.com>
2019-08-24 15:11:18 +0200

    Implement Bellman-Ford-Moore algorithm simulation

---
+++ src/main/java/unibo/algat/algorithm/BFMAlgorithm.java
@@ -0,0 +1,53 @@
+package unibo.algat.algorithm;
+
+import unibo.algat.graph.Node;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+import static unibo.algat.algorithm.SerialExecutor.runAndWait;
+
+
+/**
+ * Bellman-Ford-Moore serial algorithm implementation.
+ */
+public class BFMAlgorithm extends ShortestPathAlgorithm {
+	@Override
+	protected Void call() throws Exception {
+		final Queue<Node<Double>> q = new PriorityQueue<>(
+			mGraph.nodes().size()
+		);
+
+		runAndWait(() -> {
+			for (Node<Double> u: mGraph.nodes()) {
+				if (!u.equals(mRoot))
+					u.setData(Double.POSITIVE_INFINITY);
+			}
+
+			mRoot.setData(0.0);
+		});
+
+		q.add(mRoot);
+
+		setBreakpoint();
+
+		while (!q.isEmpty()) {
+			Node<Double> u = q.remove();
+
+			for (Node<Double> v: mGraph.adjacents(u)) {
+				setBreakpoint();
+
+				if (u.getData() + mWeights.weight(u, v) < v.getData()) {
+					if (!q.contains(v))
+						q.add(v);
+
+					// T[v] <- u
+					runAndWait(() ->
+						v.setData(u.getData() + mWeights.weight(u, v))
+					);
+				}
+			}
+		}
+
+		return null;
+	}
+}
